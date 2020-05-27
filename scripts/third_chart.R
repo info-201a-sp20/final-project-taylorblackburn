3c5e2841b796e7c209503db9c1372d1d27243825
kaisanj <kaisanj@uw.edu>
2020-05-26 19:25:24 -0700

    added chart

--- scripts/third_chart.R
+++ scripts/third_chart.R
@@ -1,11 +1,14 @@
 library(ggplot2)
 library(dplyr)
 
-# goal: Create plot that compares bechdel test rating to revenue of movies from 2000-2018
+# goal: Create plot that compares bechdel test rating to revenue of
+# movies from 2000-2018
 
 # load datsets
-rev <- read.csv("../data/Bechdel-master_revenue.csv", stringsAsFactors = FALSE)
-bechdel <- read.csv("../data/bechdel_test_df.csv", stringsAsFactors = FALSE)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+rev <- read.csv("../data/Bechdel-master_revenue.csv",
+                stringsAsFactors = FALSE)
+bechdel <- read.csv("../data/bechdel_test_df.csv",
+                    stringsAsFactors = FALSE)
 
 # get rid of uneccessary columns
 rev_select <- rev %>%
@@ -17,33 +20,32 @@ bechdel_select <- bechdel %>%
 # join tibbles
 joined_data <- left_join(rev_select, bechdel_select, by = c("Movie" = "title"))
 
-# filter out movies with no bechdel rating (na values), movies where revenue is equal to 0,
-# and filter out movies released before 2000 to account for inflation
+# Filter out movies with no bechdel rating (na values), movies where revenue
+# is equal to 0, and filter out movies released before 2000 to account
+# for inflation.
 filtered_data <- joined_data %>%
   filter(!is.na(rating)) %>%
   filter(Revenue > 0) %>%
   filter(Year > 1999)
 
-# find average revenue of each rating (0-3)
+# Find average revenue of mmovies with each rating (0-3) and scale down
+# revenue to make it easier to understand on the chart.
 summary_values <- filtered_data %>%
   group_by(rating) %>%
-  summarise(avg_revenue = mean(Revenue), max_rev = max(Revenue), min_rev = min(Revenue)) %>%
-  options(scipen = 10000)
+  summarise(avg_rev = mean(Revenue)) %>%
+  mutate(rev_to_scale = avg_rev / 1000000,
+         rev_to_scale = round(rev_to_scale, 1))
 
 # make plot
-rev_plot <- ggplot(data = summary_values) +
-  geom_bar(mapping = aes(x = rating, y = avg_revenue), position = stacking) +
-  ggtitle("Revenue of MBechdel Tested Movies") +
-  xlab("Rating") +
-  ylab("Revenue") +
-  scale_y_continuous()
-
-  ggplot( aes(x = rating, y = n, fill = name)) +
-  geom_bar(stat = "identity", width = 0.5) +
-  scale_fill_viridis(discrete = TRUE, name = "") +
-  theme_ipsum() +
-  ylab("Number of baby")
-  
-  
-
-  
+ggplot(data = summary_values, aes(x = rating, y = rev_to_scale)) +
+  geom_bar(stat = "identity", fill = "pink") +
+  geom_text(aes(label = rev_to_scale), vjust = 1.6, color = "white",
+            size = 8) +
+  theme_minimal() +
+  ggtitle("Average Revenue of Movies Released from
+          2000-2018 vs Bechdel Test Rating") +
+  xlab("Bechdel Rating") + ylab("Average Revenue (million)") +
+  ylim(0, 60) +
+  theme(axis.text = element_text(size = 11)) +
+  theme(axis.title = element_text(size = 14)) +
+  theme(plot.title = element_text(size = 17))
