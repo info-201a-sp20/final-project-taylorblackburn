bde398c1ec18c15d0ed55103c3df3cf06ec4059c
Taylor Blackburn <taylorj.blackburn@gmail.com>
2020-06-02 20:11:58 -0700

    summary

--- app.R
+++ app.R
@@ -2,4 +2,3 @@ source("app_ui.R")
 source("app_server.R")
 
 shinyApp(ui = ui, server = server)
-
--- summary.R
+++ summary.R
@@ -28,7 +28,7 @@ oldest_pass <- bechdel_rating %>%
   filter(year == min(year)) %>%
   pull(title)
 
-# Summary Takeaway #2 
+# Summary Takeaway #2 Background
 oldest_year <- bechdel_data %>%
   filter(Year == min(Year)) %>%
   filter(rating == 3) %>%
@@ -39,23 +39,54 @@ newest_year <- bechdel_data %>%
   filter(Movie == "Despicable Me 3") %>%
   pull(Year)
 
+# Summary Takeaway #3 Background
+most_revenue <- bechdel_data %>%
+  filter(rating == 3) %>%
+  filter(Revenue == max(Revenue)) %>%
+  pull(Movie)
+
 # Creating the conclusion page
 summary_takeaways <- tabPanel("Summary Takeaways",
   h1("Takeaway #1"),
-  p("One noteable observation that was gathered from our project was
-    not to judge a movie based upon its stereotype. For example,", strong(oldest_pass),
-    "was the oldest movie from this dataset to pass the Bechdel test. When a
-    person thinks of Disney princess movies, they do not automatically envision
-    the film embodying women empowerment. However, despite it's presumed stereotype,
-    it was the earliest of its kind to break societal norms."),
+  p("One noteable observation that we gathered from our project was the
+    abilitiy to not to judge a movie based upon its stereotype, particularly
+    pertaining towards gender. For example,", strong(oldest_pass),
+    "was the oldest movie from our merged dataset to pass the Bechdel test.
+    When a person thinks of Disney princess movies, they do not automatically
+    envision the film embodying women empowerment. However, despite it's
+    presumed stereotype, it was the earliest of its kind to break
+    societal norms."),
   h1("Takeaway #2"),
-  p("Our datasets ranged from", 
+  p("Our merged dataset ranges from",
   strong(oldest_year), "to", strong(newest_year),
-  ", it was particularly interesting to examine how movies tended to have a higher
-  Bechdel rating as time went on (as shown in",
+  ", it was particularly interesting to examine how movies tended to
+  have a higher Bechdel rating as time went on (as shown in",
   em("The Bechdel Test across Decades"),
-  "tab.) This allows us to think about what factors in the real world that could
-  have caused these progressive changes in the the fictional world and the inclusivity
-  of women. They are now recognized as important characters and necessary for plot development.")
+  "tab.) This allows us to think about what real-world factors
+  occuring in that particular decade that could
+  have caused progressive changes in the the fictional world of film
+  and developing the inclusivity of women. They are now
+  recognized as important characters and necessary for plot development."),
+  h1("Takeaway #3"),
+  p("Lastly, it was important to correlate the average revenue that a movie
+    produces compared to how likely it is for the movie to pass
+    the Bechdel Test. After doing some data analysis (as shown in",
+    em("Average Revenue of Movies Released from 2000-2018 vs Bechdel
+       Test Score"), "tab), it was valuable to see that movies that scored
+    a (1) on the Bechdel Test (didn't pass), generated the most revenue.
+    This is englightening as it shows the variety of other factors that could
+    have directed a movie's success upon its release. For example,",
+    strong(most_revenue), "was a relevant and powerful movie that narrarated
+    a widely-know catastrophe. Its success did not derive from its stance on
+    women empowerment, rather its sheer popularity amongst a wide variety of
+    people. Also, this data provided that the Bechdel Test is a basic meausre of
+    a film's ability for including women in fictional media; it is not a perfect
+    feminist litmus test. Films that do not pass may still dispel gender
+    stereotypes that exist outside conventional and outdated storylines.
+    On the other hand, somefilms that pass may involve an insignificant
+    converstation between two women. And while the Bechdel Test is perhaps
+    the most well-known test, this gives motivation to explore other metrics
+    that also aim to measure inclusivity in film. The combination of these tests
+    give rise to address stereotypes against race, ethnicity, and gender
+    and begin debunking them.")
 )
-  
